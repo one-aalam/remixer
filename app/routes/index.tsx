5f42ef1135efd11d2a7935c58f607aea5455e162
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-01-30 12:04:27 +0530

    2-a. Router Outlet - Default /index route

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+
+import HelloWorld from '~/components/HelloWorld'
+
+export default function Index() {
+    return (
+        <div className="min-h-screen flex flex-col justify-center">
+            <main>
+                {/* Content */}
+                <HelloWorld/>
+            </main>
+        </div>
+    )
+}
